ca6b09185fa7720b0046fa50c8aafbcb6f4916a6
Neonx <fabiangomes2015@gmail.com>
2023-10-19 09:51:38 -0500

    Nav Bad

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,14 @@
 import 'package:flutter/material.dart';
 
-class Main extends StatelessWidget {
-  const Main({super.key});
+void main() {
+  runApp(const Master());
+}
+
+class Master extends StatelessWidget {
+  const Master({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Felicho();
-  }
-}
-
-class Felicho extends StatefulWidget {
-  const Felicho({super.key});
-
-  @override
-  State<Felicho> createState() => _Felicho();
-}
-
-class _Felicho extends State<Felicho> {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-        child: Scaffold(
-      appBar: AppBar(
-        title: const Text('Por cada gay q lea me debe un beso'),
-      ),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Gay'),
-          Text('Gay'),
-          Text('Gay'),
-          Text('Gay'),
-          Text('Gay'),
-          Text('Gay'),
-          Text('Gay'),
-          Text('Gay'),
-          Text('Gay'),
-          Text('Gay'),
-          Text('Gay'),
-        ],
-      ),
-    ));
+    return const MaterialApp(home: );
   }
 }
